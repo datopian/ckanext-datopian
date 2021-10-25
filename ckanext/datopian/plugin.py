b421bd5414482551377b27fa3dc1f203d4eea0ba
Tomiwa Obanla <obanlatomiwa@gmail.com>
2021-10-25 16:34:02 +0100

    ckan theme

--- ckanext/datopian/plugin.py
+++ ckanext/datopian/plugin.py
@@ -1,9 +1,15 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from flask import Blueprint, render_template
+
+
+def hello_plugin():
+    return u'Hello from the Datopian Theme extension'
 
 
 class DatopianPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IBlueprint)
 
     # IConfigurer
 
@@ -11,4 +17,15 @@ class DatopianPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic',
-            'datopian')
+                             'datopian')
+
+    # IBlueprint
+
+    def get_blueprint(self):
+        u'''Return a Flask Blueprint object to be registered by the app.'''
+        # Create Blueprint for plugin
+        blueprint = Blueprint(self.name, self.__module__)
+        blueprint.template_folder = u'templates'
+        # Add plugin url rules to Blueprint object
+        blueprint.add_url_rule('/hello_plugin', '/hello_plugin', hello_plugin)
+        return blueprint
